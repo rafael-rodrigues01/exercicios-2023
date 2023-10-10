5aad5d46a1efc911a77e49088e09d74105a61e66
Rafael <rafa_rodrigues01@yahoo.com>
2023-10-10 09:03:31 -0300

    chore: adicionado BrowserModule, AppRoutingModule e CommonModule aos imports do AppModule importado AppRoutingModule e o CommonModule

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
