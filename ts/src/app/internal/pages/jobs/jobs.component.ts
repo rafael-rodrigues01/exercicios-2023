cfcfbfc25f923269cc1498831f1806dd6c09e4f6
Rafael <rafa_rodrigues01@yahoo.com>
2023-10-14 11:12:40 -0300

    feat: adicionado lógica para o clique no link para exibir mais ou menos texto.

--- ts/src/app/internal/pages/jobs/jobs.component.ts
+++ ts/src/app/internal/pages/jobs/jobs.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-jobs',
   templateUrl: './jobs.component.html',
-  styleUrls: ['./jobs.component.scss']
+  styleUrls: ['./jobs.component.scss'],
 })
 export class JobsComponent implements OnInit {
+  showContent: boolean = false;
+  linkText: string = 'ver mais';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showMoreContent(event: Event): void {
+    event.preventDefault();
+
+    const showContentIsTrue: boolean = !this.showContent;
+
+    this.linkText = showContentIsTrue ? 'ver menos' : 'ver mais';
+
+    if (!this.showContent) {
+      this.showContent = true;
+    } else {
+      this.showContent = false;
+    }
   }
-
 }
