c42d6373ee979794c5bc3762944040932b2022f2
Rafael <rafa_rodrigues01@yahoo.com>
2023-10-15 20:48:33 -0300

    feat: realizado novos imports

--- ts/src/app/internal/pages/jobs/jobs.module.ts
+++ ts/src/app/internal/pages/jobs/jobs.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { JobsRoutingModule } from './jobs-routing.module';
 import { JobsComponent } from './jobs.component';
+import { FormsModule } from '@angular/forms';
+import { TopicComponent } from './topic/topic.component';
 
 
 @NgModule({
   declarations: [
-    JobsComponent
+    JobsComponent,
+    TopicComponent
   ],
   imports: [
     CommonModule,
-    JobsRoutingModule
+    JobsRoutingModule,
+    FormsModule
   ]
 })
 export class JobsModule { }
