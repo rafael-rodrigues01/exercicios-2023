ff29eb2e9e2f52a4d21c17ade10c4abca25d8ac0
Rafael <rafa_rodrigues01@yahoo.com>
2023-10-15 16:52:13 -0300

    feat: adicionado métodos para tornar a página dinâmica e implementado a lógica

--- ts/src/app/internal/pages/jobs/jobs.component.ts
+++ ts/src/app/internal/pages/jobs/jobs.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobsComponent implements OnInit {
   showContent: boolean = false;
+  showForm: boolean = false;
+  showTopic: boolean = false;
   linkText: string = 'ver mais';
+  valorDoInput: string = ''
 
   constructor() {}
 
@@ -25,5 +28,24 @@ export class JobsComponent implements OnInit {
     } else {
       this.showContent = false;
     }
+  }
+
+  toggleElement() :void {
+    this.showForm = !this.showForm;
+    this.showContent = !this.showContent;
+    
+  }
+
+  showAnsweredTopic (event: Event): void {
+    event.preventDefault();
+
+    this.showForm = !this.showForm;
+    this.showTopic = false;
+    this.showTopic = !this.showTopic;
+  }
+
+  showTopicInfo():void {
+    this.showForm = !this.showForm;
+    this.showTopic = !this.showTopic;
   }
 }
